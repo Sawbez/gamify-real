042d4c0687411db6231ab0a1a0c5a90f79e74524
Sawbez <63512091+Sawbez@users.noreply.github.com>
2024-03-02 12:47:41 -0800

    redo everything

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['STATIC_FOLDER'] = "/front/dist/assets"
+app.config['TEMPLATES_FOLDER'] = "/front/dist"
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
