7a2e22f7b7012c727407f8151f6895ffe6e49e6d
Sawbez <63512091+Sawbez@users.noreply.github.com>
2024-03-02 12:54:03 -0800

    fix stuff

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
-app.config['STATIC_FOLDER'] = "/front/dist/assets"
-app.config['TEMPLATES_FOLDER'] = "/front/dist"
+dist = Path.joinpath(Path(__file__).parent, "front/dist")
+app = Flask(__name__, static_folder=dist / "assets", template_folder=dist)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
